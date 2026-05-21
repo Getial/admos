8b0f448b81e19534da3032662bd2c36bd36579d9
Felipe <bfgetial@gmail.com>
2026-05-20 21:57:40 -0500

    agrega SECURE_PROXY_SSL_HEADER y CSRF_TRUSTED_ORIGINS para proxy de Render

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -21,6 +21,12 @@ CORS_ALLOWED_ORIGINS = config(
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = config(
+    'CORS_ALLOWED_ORIGINS',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
+
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
